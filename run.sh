c6468d5ad5eaa4447448772e7f5fed1e1f698a8b
Saurabh Dash <saurabhdash2512@gmail.com>
2022-03-10 20:20:51 -0500

    final

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-mode = 'all' # 'all' uploads subfolders of the current directory and single uploads current directory as a single tar file
-token_file = '/hdd2/extra_home/sdash38/dropbox_keys/backup.txt' # location of the dropbox token file
-source_dir = '' # source directory to be backed up
-num_processes = 1 # number of processes to use for parallel uploads
+mode='' # 'all' uploads subfolders of the current directory and single uploads current directory as a single tar file
+token_file='' # location of the dropbox token file
+source_dir='' # source directory to be backed up
+num_processes=1 # number of processes to use for parallel uploads
 
-python3 -m venv run_env
-run_env/bin/pip install -r requirements.txt
+# python3 -m venv run_env
+# run_env/bin/pip install -r requirements.txt
 run_env/bin/python3 main.py --mode=$mode --token_file=$token_file --source_dir=$source_dir --num_processes=$num_processes
