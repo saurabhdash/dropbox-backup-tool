4c680d8828c91287f09c5bed1f8a8899fa094669
Saurabh Dash <saurabhdash2512@gmail.com>
2022-03-10 20:21:17 -0500

    final

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ token_file='' # location of the dropbox token file
 source_dir='' # source directory to be backed up
 num_processes=1 # number of processes to use for parallel uploads
 
-# python3 -m venv run_env
-# run_env/bin/pip install -r requirements.txt
+python3 -m venv run_env
+run_env/bin/pip install -r requirements.txt
 run_env/bin/python3 main.py --mode=$mode --token_file=$token_file --source_dir=$source_dir --num_processes=$num_processes
